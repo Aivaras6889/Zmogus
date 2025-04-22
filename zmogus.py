68c75ddb9f91a510025d0ba48fb0bc594098f627
Aivaras6889 <aivaras.zibalis@codeacademylt.onmicrosoft.com>
2025-04-22 11:26:16 +0300

    class Zmogus Ivesties pridejimas

--- zmogus.py
+++ zmogus.py
@@ -11,3 +11,42 @@ class Zmogus:
         return f"{self.vardas} {self.pavarde}, {self.amzius} m., {self.lytis}, {self.lokacija}"
     
     
+
+
+    #vardas, pavarde, amzius ,vieta ,lytis
+
+duomenys = []
+
+def IvestiDuomenys():
+
+    vardas= input("Iveskite savo varda: ")
+
+    if vardas.lower() == "q":
+        return "q", "", "", 0, ""
+        
+    pavarde = input("Iveskit savo pavarde: ")
+    lytis = input("Iveskite savo lyti: ")
+    amzius = int(input("Iveskite savo amziu: "))
+    vieta = input("Iveskite vietove kurioje gyvenate(Miestas/Kaimas ...) ")
+
+    return vardas, pavarde, lytis, amzius, vieta
+
+
+
+while True:
+    print("Iveskite duomenys(vardas, pavarde, lytis, amzius, vieta) noredami uzbaigti iveskite 'q' ")
+
+    vardas, pavarde, lytis, amzius, vieta = IvestiDuomenys()
+
+    if vardas == "q":
+        break
+
+    asmuo = Zmogus(vardas, pavarde, lytis, amzius, vieta)
+    duomenys.append(asmuo)
+
+sarasas = duomenys()
+print(sarasas)
+
+
+
+
