e80c48509fd8e364c126184d2a0c1a815ec3a1b0
Mildasita <milda@dabasinskas.net>
2025-04-22 10:48:58 +0300

    class Zmogus

---
+++ zmogus.py
@@ -0,0 +1,13 @@
+class Zmogus:
+    def __init__(self, vardas, pavarde, amzius, lytis, lokacija):
+        self.vardas = vardas
+        self.pavarde = pavarde
+        self.amzius = amzius
+        self.lytis = lytis
+        self.lokacija = lokacija
+
+
+    def __str__(self):
+        return f"{self.vardas} {self.pavarde}, {self.amzius} m., {self.lytis}, {self.lokacija}"
+    
+    
