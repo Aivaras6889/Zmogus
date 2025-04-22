d793a1234fd456d32bfc215d8163d583fae93c43
Aivaras6889 <aivaras.zibalis@codeacademylt.onmicrosoft.com>
2025-04-22 12:55:00 +0300

    Pakeista

--- zmogus.py
+++ zmogus.py
@@ -5,48 +5,45 @@ class Zmogus:
         self.amzius = amzius
         self.lytis = lytis
         self.lokacija = lokacija
-
-
     def __str__(self):
         return f"{self.vardas} {self.pavarde}, {self.amzius} m., {self.lytis}, {self.lokacija}"
     
+    def atvaizduoti_zmones(zmoniu_sar):
+        rezultatas="\n Žmonių sąrašas:\n"
+        for zmogus in zmoniu_sar:
+            rezultatas += f"{zmogus}"
+        return rezultatas
 
 
+    def skaiciuoti_vidurki(zmoniu_sar):
+        if len(zmoniu_sar) == 0:
+            return 0
+        suma = 0
+        for zmogus in zmoniu_sar:
+            suma += zmogus.amzius
+        vidurkis = suma / len(zmoniu_sar)
+        return vidurkis
 
-    #vardas, pavarde, amzius ,vieta ,lytis
 
-duomenys = []
+zmoniu_sar = []
 
-def IvestiDuomenys():
+while True:
+    print("Iveskite duomenys(vardas, pavarde, lytis, amzius, vieta) noredami uzbaigti iveskite 'q' ")
 
     vardas= input("Iveskite savo varda: ")
 
     if vardas.lower() == "q":
-        return "q", "", "", 0, ""
-        
+        break
     pavarde = input("Iveskit savo pavarde: ")
     lytis = input("Iveskite savo lyti: ")
     amzius = int(input("Iveskite savo amziu: "))
     vieta = input("Iveskite vietove kurioje gyvenate(Miestas/Kaimas ...) ")
 
-    return vardas, pavarde, lytis, amzius, vieta
-
-
-
-while True:
-    print("Iveskite duomenys(vardas, pavarde, lytis, amzius, vieta) noredami uzbaigti iveskite 'q' ")
-
-    vardas, pavarde, lytis, amzius, vieta = IvestiDuomenys()
-
-    if vardas == "q":
-        break
-
     asmuo = Zmogus(vardas, pavarde, lytis, amzius, vieta)
-    duomenys.append(asmuo)
+    zmoniu_sar.append(asmuo)
 
-sarasas = duomenys()
-print(sarasas)
 
+    
 
     
     
