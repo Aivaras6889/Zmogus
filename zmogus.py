ad311cf6a95f8aa30121286ec48ff490f3702b75
Mildasita <milda@dabasinskas.net>
2025-04-22 11:02:31 +0300

    pridejau zmoniu saraso atvaizdavima

--- zmogus.py
+++ zmogus.py
@@ -10,4 +10,14 @@ class Zmogus:
     def __str__(self):
         return f"{self.vardas} {self.pavarde}, {self.amzius} m., {self.lytis}, {self.lokacija}"
     
+
+
+    def atvaizduoti_zmones(zmoniu_sar):
+        print("\n Žmonių sąrašas:")
+        for zmogus in zmoniu_sar:
+            print(zmogus)
+
+
     
+    
+
